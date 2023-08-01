e982925cfc57e2544993f185b7d3728002461c6c
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-08-01 16:05:27 -0400

    Added a helper folder with utils attached, may not use in the end

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,33 @@
-const router = require('express').Router;
+const notes = require('express').Router;
+const { readFile } = require("fs").promises;
+
+// const {
+//   readFromFile,
+//   readAndAppend,
+//   writeToFile,
+// } = require('../helpers/fsUtils');
+
+
+// Make a get, post and a delete here
+
+// GET Route for retreiving all notes
+notes.get('/', (req, res) => {
+  readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+// notes.get('/', (req, res) => {
+//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// });
+
+// GET Route for a specific note
+// notes.get('/:')
+
+
+// everything with /api/notes needs to be done here
+// (e.g., posts/gets from server)
+// look at the fetch requests in index
+// 
+
+module.exports = notes;
+
