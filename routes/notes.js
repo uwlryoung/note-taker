05c6c9571ec4d22372910d99994d66c0b17d9b88
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-08-01 17:14:02 -0400

    Added a POST route to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router;
-const { readFile } = require("fs").promises;
+const { readFile, writeFile } = require("fs").promises;
+const { v4: uuidv4 } = require('uuid');
 
 // const {
 //   readFromFile,
@@ -15,6 +16,9 @@ notes.get('/', (req, res) => {
   readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// GET Route for a specific note
+notes.get('/:')
+
 
 // notes.get('/', (req, res) => {
 //   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
@@ -28,6 +32,33 @@ notes.get('/', (req, res) => {
 // (e.g., posts/gets from server)
 // look at the fetch requests in index
 // 
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { username, topic, note} = req.body;
+
+  if (req.body) {
+    const newNote = {
+      username,
+      tip,
+      topic,
+      note_id: uuidv4(),
+    }
+    readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeFile('/db/db.json', parsedData);
+      }
+    });
+  } else {
+    res.error('Error in adding note');
+  }
+})
 
 module.exports = notes;
 
